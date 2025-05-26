f26abb34e604a72b40410f08179dded10126613b
anonymous298 <tackletalha@gmail.com>
2025-05-27 04:00:58 +0500

    js updated

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ if (searchBox.contains(e.target)) {
 }
 });
 
+// Creating logic for hamburger menu 
+let hamburgerButton = document.querySelector('.hamburger');
+let closeButton = document.querySelector('.close');
+
+hamburgerButton.addEventListener('click', () => {
+    document.querySelector('aside').classList.toggle('show');
+    document.querySelector('.close').classList.toggle('show')
+    document.querySelector('.aside-inner-text').classList.toggle('show')
+})
+
+
+closeButton.addEventListener('click', () => {
+    document.querySelector('aside').classList.toggle('show');
+    document.querySelector('.close').classList.toggle('show')
+    document.querySelector('.aside-inner-text').classList.toggle('show')
+})
